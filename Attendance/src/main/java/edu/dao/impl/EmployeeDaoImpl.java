b3b6dfd62fff6cc10684e4a217d556c5761757ed
Ugly-Hang <354058255@qq.com>
2019-06-26 14:01:49 +0800

    Revert "部门表优化"
    This reverts commit 49477b73

---
+++ Attendance/src/main/java/edu/dao/impl/EmployeeDaoImpl.java
@@ -0,0 +1,476 @@
+package edu.dao.impl;
+
+import com.liuvei.common.DbFun;
+import edu.bean.Employee;
+import edu.dao.EmployeeDao;
+import edu.util.DbUtil.DbUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDaoImpl implements EmployeeDao {
+    @Override
+    public List<Employee> list() {
+        List<Employee> list = new ArrayList<Employee>();
+
+        StringBuffer sbSQL = new StringBuffer();
+        List<Object> paramsList = new ArrayList<Object>();
+
+        sbSQL.append("select * from Employee");
+
+        String sql = sbSQL.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        ResultSet rs = null;
+        try {
+            conn = DbUtil.getConn();
+
+            rs = DbFun.query(conn, sql, params);
+
+            while (rs.next()) {
+                list.add(toBean(rs));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            DbUtil.close(conn);
+        }
+        return list;
+    }
+
+    @Override
+    public Long insert(Employee bean) {
+        // TODO Auto-generated method stub
+        Long result = 0L;
+
+        // 0)定义变量
+        StringBuffer sql = new StringBuffer();
+        List<Object> paramsList = new ArrayList<Object>();
+
+        // 1)组合SQL
+        sql.append(" Insert Into Employee");
+        sql.append(" (");
+        sql.append(" empCode,empName,sex,age");
+        sql.append(" ,nation,IDC,money,tel");
+        sql.append(" ,ecp,ecpTel,jobId,des");
+        sql.append(" )");
+        sql.append(" values(?,?,?,? ,?,?,?,? ,?,?,?,? )");
+
+        // 2)添加参数
+        paramsList.add(bean.getEmpCode());
+        paramsList.add(bean.getEmpName());
+        paramsList.add(bean.getSex());
+        paramsList.add(bean.getAge());
+
+        paramsList.add(bean.getNation());
+        paramsList.add(bean.getIDC());
+        paramsList.add(bean.getMoney());
+        paramsList.add(bean.getTel());
+
+        paramsList.add(bean.getEcp());
+        paramsList.add(bean.getEcpTel());
+        paramsList.add(bean.getJobId());
+        paramsList.add(bean.getDes());
+
+
+        // 3)转换类型
+        String SQL = sql.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        try {
+            // 4)取得连接对象
+            conn = DbUtil.getConn();
+
+            // 5)执行SQL
+            Long num = DbFun.update(conn, SQL, params);
+
+            // 6)结果处理
+            if (num > 0) {
+                SQL = "Select @@identity";
+                result = DbFun.queryScalarLong(conn, SQL);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            // 9)关闭连接
+            DbUtil.close(conn);
+        }
+        return result;
+    }
+
+    @Override
+    public Long delete(Long id) {
+        // TODO Auto-generated method stub
+
+        Long result = 0L;
+
+        // 0）定义变量
+        StringBuffer sql = new StringBuffer();
+        List<Object> paramsList = new ArrayList<Object>();
+
+        // 1) 组合SQL
+        sql.append(" delete from Employee");
+        sql.append(" where empId=?");
+
+        // 2) 添加参数
+        paramsList.add(id);
+
+        // 3) 转换类型
+        String SQL = sql.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        try {
+            // 4) 取得连接对象
+            conn = DbUtil.getConn();
+
+            // 5) 执行sql
+            result = DbFun.update(conn, SQL, params);
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            // 9) 关闭连接
+            DbUtil.close(conn);
+        }
+
+        return result;
+
+
+    }
+
+    @Override
+    public Long update(Employee bean) {
+        // TODO Auto-generated method stub
+
+        Long result = 0L;
+
+        // 0) 定义变量
+        StringBuffer sbSQL = new StringBuffer();
+        List<Object> paramsList = new ArrayList<Object>();
+
+        // 1) 组合SQL
+        sbSQL.append(" update Employee ");
+        sbSQL.append(" set empCode=?, empName=?, sex=?, age=?");
+        sbSQL.append(" , nation=?, IDC=?, money=?, tel=?");
+        sbSQL.append(" , ecp=?, ecpTel=?, jobId=?, des=?");
+        sbSQL.append(" where empId=?");
+
+        // 2) 添加参数
+        paramsList.add(bean.getEmpCode());
+        paramsList.add(bean.getEmpName());
+        paramsList.add(bean.getSex());
+        paramsList.add(bean.getAge());
+
+        paramsList.add(bean.getNation());
+        paramsList.add(bean.getIDC());
+        paramsList.add(bean.getMoney());
+        paramsList.add(bean.getTel());
+
+        paramsList.add(bean.getEcp());
+        paramsList.add(bean.getEcpTel());
+        paramsList.add(bean.getJobId());
+        paramsList.add(bean.getDes());
+
+        paramsList.add(bean.getEmpId());
+
+        // 3)转换类型
+        String sql = sbSQL.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        try {
+            // 4)取得连接对象
+            conn = DbUtil.getConn();
+
+            // 5)执行SQL
+            result = DbFun.update(conn, sql, params);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            // 9) 关闭连接
+            DbUtil.close(conn);
+        }
+        return result;
+    }
+
+    @Override
+    public Employee load(Long id) {
+        Employee bean = null;
+        //定义变量
+        StringBuffer sbSQL = new StringBuffer();
+        List<Object> paramsList = new ArrayList<Object>();
+        //组合SQL
+        sbSQL.append("Select * from Employee");
+        sbSQL.append(" where empId=?");
+
+        //添加参数
+        paramsList.add(id);
+
+        //类型转换
+        String sql = sbSQL.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        ResultSet rs =null;
+
+
+        try {
+            //取得连接对象
+            conn = DbUtil.getConn();
+
+            //执行sql
+            rs=DbFun.query(conn,sql,params);
+
+            //单行转换为对象
+
+            if(rs.next()){
+                bean =toBean(rs);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw  new RuntimeException(e);
+        }finally {
+            //关闭
+            DbUtil.close(conn);
+        }
+        return bean;
+    }
+
+    @Override
+    public Employee loadByName(String name) {
+        Employee bean = null;
+        //定义变量
+        StringBuffer sbSQL = new StringBuffer();
+        List<Object> paramsList = new ArrayList<Object>();
+        //组合SQL
+        sbSQL.append("select * from Employee ");
+        sbSQL.append("where empName= ? ");
+        sbSQL.append(" order by empId asc");
+
+        //添加参数
+        paramsList.add(name);
+
+        //类型转换
+        String sql = sbSQL.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        ResultSet rs =null;
+
+
+        try {
+            //取得连接对象
+            conn = DbUtil.getConn();
+
+            //执行sql
+            rs=DbFun.query(conn,sql,params);
+
+            //单行转换为对象
+
+            if(rs.next()){
+                bean =toBean(rs);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw  new RuntimeException(e);
+        }finally {
+            //关闭
+            DbUtil.close(conn);
+        }
+        return bean;
+    }
+
+    @Override
+    public Long count() {
+        Long result =0L;
+
+        StringBuffer sbSQL = new StringBuffer();
+        List<Object> paramsList = new  ArrayList<Object>();
+
+        sbSQL.append("select count(1) From Employee");
+
+        String sql =sbSQL.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        try {
+            //取得连接对象
+            conn = DbUtil.getConn();
+
+            //执行sql
+            result=DbFun.queryScalarLong(conn,sql,params);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw  new RuntimeException(e);
+        }finally {
+            //关闭
+            DbUtil.close(conn);
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<Employee> pager(Long pageNum, Long pageSize) {
+        List<Employee> list = new  ArrayList<Employee>();
+
+        StringBuffer sbSQL = new StringBuffer();
+        List<Object> paramsList = new  ArrayList<Object>();
+
+        sbSQL.append("select * from Employee");
+        sbSQL.append(" order by empId asc");
+
+        if(pageNum<1){
+            pageNum=1L;
+        }
+        if(pageSize<1){
+            pageSize=10L;
+        }
+        Long startIndex = (pageNum - 1)*pageSize;
+        sbSQL.append(" limit "+startIndex+","+pageSize);
+
+        String sql =sbSQL.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        ResultSet rs = null;
+        try {
+            conn = DbUtil.getConn();
+
+            rs = DbFun.query(conn, sql, params);
+
+            while (rs.next()) {
+                list.add(toBean(rs));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            DbUtil.close(conn);
+        }
+        return list;
+    }
+
+    @Override
+    public Long countByName(String name) {
+        Long result =0L;
+
+        StringBuffer sbSQL = new StringBuffer();
+        List<Object> paramsList = new  ArrayList<Object>();
+
+        sbSQL.append("select count(1) From Employee");
+        sbSQL.append(" where empName like ? ");
+
+        name = "%" + name + "%";
+        paramsList.add(name);
+
+        String sql =sbSQL.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        try {
+            //取得连接对象
+            conn = DbUtil.getConn();
+
+            //执行sql
+            result=DbFun.queryScalarLong(conn,sql,params);
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw  new RuntimeException(e);
+        }finally {
+            //关闭
+            DbUtil.close(conn);
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<Employee> pagerByName(String name, Long pageNum, Long pageSize) {
+        List<Employee> list = new  ArrayList<Employee>();
+
+        StringBuffer sbSQL = new StringBuffer();
+        List<Object> paramsList = new  ArrayList<Object>();
+
+        sbSQL.append("select * from Employee");
+        sbSQL.append(" where empName like ?");
+        sbSQL.append(" order by empId asc");
+
+        if(pageNum<1){
+            pageNum=1L;
+        }
+        if(pageSize<1){
+            pageSize=10L;
+        }
+        Long startIndex = (pageNum - 1)*pageSize;
+        sbSQL.append(" limit "+startIndex+","+pageSize);
+
+        name="%"+name+"%";
+        paramsList.add(name);
+
+        String sql =sbSQL.toString();
+        Object[] params = paramsList.toArray();
+
+        Connection conn = null;
+        ResultSet rs = null;
+        try {
+            conn = DbUtil.getConn();
+
+            rs = DbFun.query(conn, sql, params);
+
+            while (rs.next()) {
+                list.add(toBean(rs));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            DbUtil.close(conn);
+        }
+        return list;
+    }
+    private Employee toBean(ResultSet rs) {
+        Employee bean = new Employee();
+
+        try {
+            bean.setEmpId(rs.getLong("empId"));
+            bean.setEmpCode(rs.getString("empCode"));
+            bean.setEmpName(rs.getString("empName"));
+            bean.setSex(rs.getString("sex"));
+            bean.setAge(rs.getLong("age"));
+            bean.setNation(rs.getString("nation"));
+            bean.setIDC(rs.getString("IDC"));
+            bean.setMoney(rs.getLong("money"));
+            bean.setTel(rs.getString("tel"));
+            bean.setEcp(rs.getString("ecp"));
+            bean.setEcpTel(rs.getString("ecpTel"));
+            bean.setJobId(rs.getLong("jobId"));
+            bean.setDes(rs.getString("des"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        return bean;
+    }
+}
---
+++ Attendance/src/main/java/edu/ui/cli/test.java
@@ -0,0 +1,81 @@
+package edu.ui.cli;
+
+
+
+import edu.bean.Employee;
+import edu.service.impl.EmployeeService;
+import edu.service.impl.impl.EmployeeServiceImpl;
+
+import java.nio.file.WatchKey;
+import java.util.List;
+
+public class test {
+    private static  EmployeeService employeeService = new EmployeeServiceImpl();
+
+    public static void main(String[] args) {
+        list();
+       //delete();
+       // insert();
+       // load();
+        //update();
+        //L();
+    }
+
+
+    private static void L(){
+        List<Employee> list = employeeService.pagerByName("w",2L,1L);
+        for (Employee item : list) {
+            System.out.println(item.getEmpId() + "\t");
+            System.out.println(item.getEmpName() + "\t");
+            System.out.println(item.getEmpCode() + "\n");
+        }
+    }
+
+
+
+
+    private static void list() {
+        List<Employee> list = employeeService.list();
+        for (Employee item : list) {
+            System.out.println(item.getEmpId() + "\t");
+            System.out.println(item.getEmpName() + "\t");
+            System.out.println(item.getEmpCode() + "\n");
+        }
+
+
+    }
+
+    private static void delete() {
+        System.out.println(employeeService.delete(4L));
+    }
+    private static void load(){
+        Employee item = employeeService.load(3L);
+        System.out.println(item.getEmpId() + "\t");
+        System.out.println(item.getEmpName() + "\t");
+        System.out.println(item.getEmpCode() + "\n");
+
+    }
+    private static void update(){
+        Employee employee = new Employee();
+         employee.setEmpId(3L);
+        employee.setEmpCode("K");
+        employee.setEmpName("WK");
+        employee.setSex("男");
+        employee.setAge(20L);
+        employee.setJobId(1L);
+        employeeService.update(employee);
+    }
+
+    private static void insert() {
+        Employee employee = new Employee();
+        //employee.setEmpId(4L);
+        employee.setEmpCode("WK");
+        employee.setEmpName("WA");
+        employee.setSex("男");
+        employee.setAge(20L);
+
+
+        employeeService.insert(employee);
+    }
+
+}
